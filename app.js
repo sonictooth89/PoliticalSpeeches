d54ca0bc7d3e1c483df660e0b75e55d56d3d1119
Malgorzata Nowak <nowak.em@interia.eu>
2023-01-16 09:29:44 +0100

    new routes added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { homeRouter } = require('./routers/home');
 const { evaluationRouter } = require('./routers/evaluation');
+const { speech1Router, speech2Router, speech3Router } = require('./routers/speeches');
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,9 @@ app.use(express.static('public'));
 
 app.use('/home', homeRouter);
 app.use('/evaluation', evaluationRouter);
+app.use('/speech1', speech1Router);
+app.use('/speech2', speech2Router);
+app.use('/speech3', speech3Router);
 
 app.listen(3000, () => {
     console.log(`Server started: http://localhost:${port}/`);
