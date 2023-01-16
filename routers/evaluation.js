b20949ddeb5bfdacdee08c6685ea610ab72f83c0
Malgorzata Nowak <nowak.em@interia.eu>
2023-01-16 22:10:02 +0100

    merge csv function added

--- routers/evaluation.js
+++ routers/evaluation.js
@@ -1,16 +1,10 @@
 const express = require('express');
+const {readFile, writeFile} = require('fs');
+const {merge} = require('csv-merger');
 const csv = require('csvtojson');
-const path = require('path');
-const http = require('http');
-const parse = require('papaparse');
-const { pipeline } = require('fs');
-// const { readFile } = require('fs');
 const { mostSpeechesYear } = require('../utils/methodMostSpeechesYear');
 const { mostTopicSecurity } = require('../utils/methodMostTopicSecurity');
 const { leastTotalWordy } = require('../utils/methodFewestWords');
-const { createWriteStream } = require('fs');
-const { createReadStream } = require('fs');
-
 
 const evaluationRouter = express.Router();
 
@@ -24,31 +18,44 @@ evaluationRouter
                 leastWordy: null
             });
         };
+        // [ url1, url2, url3 ]
 
         const urls = Array.isArray(req.query.url)
                 ? req.query.url.map(url => String(url)) : [String(req.query.url)];
-                // [ url1, url2, url3 ]
-        
-                for(let url of urls){
+
+                (async() => {
+                    let inputFiles = [];
+                    for (let url of urls){
+                        await inputFiles.push(`./utils/csv/${url}.csv`);
+                    }
+                    const completeData = await merge(inputFiles, options = {
+                        outputPath: './utils/csv/mergeFile.csv',
+                        writeOutput: true,
+                    });
+                    console.log(completeData);
+                })();
                     
-                        const csvFilePath = `./utils/csv/${url}.csv`;
-                        csv()
+                (async() => {
+                    const csvFilePath = './utils/csv/mergeFile.csv';
+                        await csv()
                             .fromFile(csvFilePath)
                             .then((jsonObj) => {
                                 
                                 const obj = (jsonObj);
-                                
+                                console.log(jsonObj)
                                 return res.json({
                                     mostSpeeches: mostSpeechesYear(obj),
                                     mostSecurity: mostTopicSecurity(obj),
                                     leastWordy: leastTotalWordy(obj),
                                 })        
                                 });
+                })();        
                             
-                    
-            };
+            
             console.log('Program has finished!')
-        })
+        });
+
+        
 
 
 // console.log(csvData)
